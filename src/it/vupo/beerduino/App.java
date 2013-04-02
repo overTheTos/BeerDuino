64d75348bec5c65bec5f3c444b92c1ca3ffb14d0
Tommaso Righi <tmsrighi@gmail.com>
2013-04-02 07:50:58 +0200

    Popup Insert step

--- src/it/vupo/beerduino/App.java
+++ src/it/vupo/beerduino/App.java
@@ -10,10 +10,13 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.text.NumberFormat;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -218,6 +221,8 @@ public class App extends JFrame implements ActionListener {
 		}
 	}
 
+	private Object[][] data;
+	
 	public JTable createTable(){
 		
 		String[] columnNames = {"Current Step",
@@ -228,12 +233,12 @@ public class App extends JFrame implements ActionListener {
 				                "Rest Stop Temp",
 				                "Step Elapsed Time"};
 		
-		Object[][] data = {
-		        {false, "Acid Rest", 0, 15, 35, 35, 0},
-		        {false, "Protein Rest", 7, 20, 42, 42, 0},
-		        {true, "Saccharification", 24, 60, 66, 66, 0},
-		        {false, "Mash Out", 12, 15, 78, 78, 0}
-		        };
+//		data = {
+//		        {false, "Acid Rest", 0, 15, 35, 35, 0},
+//		        {false, "Protein Rest", 7, 20, 42, 42, 0},
+//		        {true, "Saccharification", 24, 60, 66, 66, 0},
+//		        {false, "Mash Out", 12, 15, 78, 78, 0}
+//		        };
 		
 		final JTable table = new JTable(data, columnNames);
         table.setPreferredScrollableViewportSize(new Dimension(500, 70));
@@ -247,6 +252,39 @@ public class App extends JFrame implements ActionListener {
         table.getColumnModel().getColumn(5).setCellRenderer( rightRenderer );
         table.getColumnModel().getColumn(6).setCellRenderer( rightRenderer );
         
+        
+        //TODO: FINIRE BUTTON 
+        JButton addRow = new JButton();
+        addRow.setText("Add");
+        addRow.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				createAndShowGUI(); 
+			}
+		});
+        
         return table;
 	}
+	
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("InputForm");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         
+        //Create and set up the content pane.
+        JComponent newContentPane = new InputForm();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+         
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
---
+++ src/it/vupo/beerduino/InputForm.java
@@ -0,0 +1,243 @@
+package it.vupo.beerduino;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+import javax.swing.BorderFactory;
+import javax.swing.InputVerifier;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class InputForm extends JPanel{
+	
+	private static int DEFAULT_VALUE = 0;
+	private NumberFormat numberFormat;
+	
+	private FormVerifier formVerifier = new FormVerifier();
+	
+	private JLabel stepNameLabel = new JLabel();
+	private JLabel stepTimeLabel = new JLabel();
+	private JLabel restTimeLabel = new JLabel();
+	private JLabel restStartTempLabel = new JLabel();
+	private JLabel restStopTempLabel = new JLabel();
+	
+	private JTextField stepNameField = new JTextField();
+	private JTextField stepTimeField = new JTextField(numberFormat.format(DEFAULT_VALUE), 5);
+	private JTextField restTimeField = new JTextField(numberFormat.format(DEFAULT_VALUE), 5);
+	private JTextField restStartTempField = new JTextField(numberFormat.format(DEFAULT_VALUE), 5);
+	private JTextField restStopTempField = new JTextField(numberFormat.format(DEFAULT_VALUE), 5);
+	
+	private JButton confirmBtn = new JButton();
+	private JButton clearBtn = new JButton();
+	private JButton exitBtn = new JButton();
+	
+	public InputForm(){
+		super(new BorderLayout());
+        setUpFormats();
+        
+		//Labels
+		stepNameLabel.setText("Step Name:");
+		stepTimeLabel.setText("Step Time:");
+		restTimeLabel.setText("Rest Time:");
+		restStartTempLabel.setText("Rest Start Temp:");
+		restStopTempLabel.setText("Rest Stop Temp:");
+		
+		//Fields
+		stepTimeField.setInputVerifier(formVerifier);
+		restTimeField.setInputVerifier(formVerifier);
+		restStartTempField.setInputVerifier(formVerifier);
+		restStopTempField.setInputVerifier(formVerifier);
+		
+		//Action listeners
+		stepTimeField.addActionListener(formVerifier);
+		restTimeField.addActionListener(formVerifier);
+		restStartTempField.addActionListener(formVerifier);
+		restStopTempField.addActionListener(formVerifier);
+		
+		//Link labels to fields
+		stepNameLabel.setLabelFor(stepNameField);
+		stepTimeLabel.setLabelFor(stepTimeField);
+		restTimeLabel.setLabelFor(restTimeField);
+		restStartTempLabel.setLabelFor(restStartTempField);
+		restStopTempLabel.setLabelFor(restStopTempField);
+		
+		//Buttons
+		confirmBtn.setText("Ok");
+		confirmBtn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Object[] dataInsert = { 
+				false,
+				stepNameField.getText(),
+				stepTimeField.getText(),
+				restTimeField.getText(),
+				restStartTempField.getText(),
+				restStopTempField.getText(),
+				0};
+				
+			}
+		});
+		clearBtn.setText("Clear");
+		exitBtn.setText("Exit");
+		
+		JPanel labelPane = new JPanel(new GridLayout(0,1));
+	    labelPane.add(stepNameLabel);
+	    labelPane.add(stepTimeLabel);
+	    labelPane.add(restTimeLabel);
+	    labelPane.add(restStartTempLabel);
+	    labelPane.add(restStopTempLabel);
+	    
+	    JPanel fieldPane = new JPanel(new GridLayout(0,1));
+	    fieldPane.add(stepNameField);
+	    fieldPane.add(stepTimeField);
+	    fieldPane.add(restTimeField);
+	    fieldPane.add(restStartTempField);
+	    fieldPane.add(restStopTempField);
+	    
+	    JPanel btnPane = new JPanel(new GridLayout(0,1));
+	    btnPane.add(confirmBtn);
+	    btnPane.add(clearBtn);
+	    btnPane.add(exitBtn);
+	    
+	    //Put the panels in this panel, labels on left,
+	    //text fields on right.
+	    setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+	    add(labelPane, BorderLayout.CENTER);
+	    add(fieldPane, BorderLayout.LINE_END);
+	    add(btnPane, BorderLayout.SOUTH);
+	}
+	
+	private void setUpFormats() {
+        numberFormat = NumberFormat.getNumberInstance();
+        numberFormat.setParseIntegerOnly(true);
+	}
+	
+    class FormVerifier extends InputVerifier
+    implements ActionListener {
+
+    	int MIN_TEMP = 1;
+    	int MAX_TEMP = 90;
+    	int MIN_TIME = 1;
+    	int MAX_TIME = 999;
+    	
+    	public boolean shouldYieldFocus(JComponent input) {
+            boolean inputOK = verify(input);
+            makeItPretty(input);
+             
+            if (inputOK) {
+                return true;
+            } else {
+                Toolkit.getDefaultToolkit().beep();
+                return false;
+            }
+        }
+    	
+    	@Override
+        public boolean verify(JComponent input) {
+            return checkField(input, false);
+        }
+         
+        protected void makeItPretty(JComponent input) {
+            checkField(input, true);
+        }
+         
+        protected boolean checkField(JComponent input, boolean changeIt) {
+            if (input == stepTimeField) {
+                return checkTimeField(changeIt, stepTimeField);
+            } else if (input == restTimeField) {
+                return checkTimeField(changeIt, restTimeField);
+            } else if (input == restStartTempField) {
+                return checkTempField(changeIt, restStartTempField);
+            } else if (input == restStopTempField) {
+                return checkTempField(changeIt, restStopTempField);
+            } else {
+                return true;
+            }
+        }
+        
+        protected boolean checkTempField(boolean change, JTextField field) {
+            boolean wasValid = true;
+            int defaultValue = DEFAULT_VALUE;
+            
+            //Parse the value.
+            try {
+            	defaultValue = numberFormat.parse(field.getText()).intValue();
+            } catch (ParseException pe) {
+                wasValid = false;
+                pe.printStackTrace();
+            }
+             
+            //Value was invalid.
+            if ((defaultValue < MIN_TEMP) || (defaultValue > MAX_TEMP)) {
+                wasValid = false;
+                if (change) {
+                    if (defaultValue < MIN_TEMP) {
+                    	defaultValue = MIN_TEMP;
+                    } else { // numPeriods is greater than MAX_PERIOD
+                    	defaultValue = MAX_TEMP;
+                    }
+                }
+            }
+             
+            //Whether value was valid or not, format it nicely.
+            if (change) {
+            	field.setText(numberFormat.format(defaultValue));
+            	field.selectAll();
+            }
+             
+            return wasValid;
+        }
+        
+        protected boolean checkTimeField(boolean change, JTextField field) {
+            boolean wasValid = true;
+            int defaultValue = DEFAULT_VALUE;
+            
+            //Parse the value.
+            try {
+            	defaultValue = numberFormat.parse(field.getText()).intValue();
+            } catch (ParseException pe) {
+                wasValid = false;
+                pe.printStackTrace();
+            }
+             
+            //Value was invalid.
+            if ((defaultValue < MIN_TIME) || (defaultValue > MAX_TIME)) {
+                wasValid = false;
+                if (change) {
+                    if (defaultValue < MIN_TIME) {
+                    	defaultValue = MIN_TIME;
+                    } else { // numPeriods is greater than MAX_PERIOD
+                    	defaultValue = MAX_TIME;
+                    }
+                }
+            }
+             
+            //Whether value was valid or not, format it nicely.
+            if (change) {
+            	field.setText(numberFormat.format(defaultValue));
+            	field.selectAll();
+            }
+             
+            return wasValid;
+        }
+        
+        @Override 
+        public void actionPerformed(ActionEvent e) {
+            JTextField source = (JTextField)e.getSource();
+            shouldYieldFocus(source); //ignore return value
+            source.selectAll();
+        }
+    }
+    
+
+}
